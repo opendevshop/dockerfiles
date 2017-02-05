1a76a48551a1d9766964f1f1359e22ac4bdcdb32
Jon Pugh <jon@thinkdrop.net>
2017-02-05 10:56:46 -0500

    Better comments.

--- run-tests.sh
+++ run-tests.sh
@@ -6,6 +6,8 @@ echo "run-tests.sh | Running  /usr/share/devshop/bin/devshop devmaster:test"
 
 # Pause the task queue.
 drush @hostmaster vset hosting_queued_paused 1
+
+# Run the test suite.
 /usr/share/devshop/bin/devshop devmaster:test
 
 # Unpause the task queue.
